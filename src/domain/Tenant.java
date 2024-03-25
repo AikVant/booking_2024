aa1ee7616b9b1dc0d98ac17b055c4b425c149d70
vankat <p3210020@aueb.gr>
2024-03-25 19:14:49 +0200

    add Reservation, ReservationDate, Tenant

---
+++ src/domain/Tenant.java
@@ -0,0 +1,24 @@
+package domain;
+
+public class Tenant {
+    private String name;
+
+    public Tenant(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Tenant{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
