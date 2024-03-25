b2b12a475941cd878bdbc59a771847095233a47e
vankat <p3210020@aueb.gr>
2024-03-25 19:22:31 +0200

    add interface Search, class SearchImplementation

---
+++ src/domain/Search.java
@@ -0,0 +1,5 @@
+package domain;
+
+public interface Search {
+    Accomodation searchAccommodations(Area area, ReservationDate date);
+}
---
+++ src/domain/SearchImplementation.java
@@ -0,0 +1,7 @@
+package domain;
+
+public class SearchImplementation implements Search{
+    public Accomodation searchAccommodations(Area area, ReservationDate date){
+        return null;
+    }
+}
